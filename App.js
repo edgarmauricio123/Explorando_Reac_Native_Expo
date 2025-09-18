43a519fd01613406b3e102d18b560b524d5424cb
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-09-17 21:07:16 -0600

    1.Inicio de practica

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text>Primer boton</Text>
     </View>
   );
 }
@@ -13,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f3a1ff',
+    padding: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    
+  
   },
 });
