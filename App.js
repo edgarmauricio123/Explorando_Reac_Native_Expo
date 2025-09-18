0b62e9a34abe0a4dc7869f6500d3f38a1c4c9d5d
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-09-17 21:51:31 -0600

    2.Realizacion de useState

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 // import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [color, setcolor] = useState('#f2c5baff');
+
+  const cambiarColor = () => {
+    const colores = ['#a4baf6ff', '#002eabff', '#a4f4cfff', '#e7adaaff'];
+    const nuevocolor = colores[Math.floor(Math.random() * colores.length)];
+    setcolor(nuevocolor)
+  };
   return (
-    <View style={styles.container}>
-     <Text>Primer boton</Text>
+    <View style={[styles.container, {backgroundColor: color}]}>
+      <Text>Cambiar color</Text>
+      <TouchableOpacity onPress={cambiarColor} style={styles.saveButton}>
+        <Text style={styles.saveButtonText}>Ver mas</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,10 +26,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f3a1ff',
     padding: 50,
     alignItems: 'center',
-    
-  
+    justifyContent: 'center',
+
+  },
+  saveButton: {
+    backgroundColor: '#a4baf6ff',
+    marginTop: 30,
+    paddingVertical: 6,
+    borderRadius: 40,
+    padding: 15
+
+  },
+  button: {
+    marginRight: 20,
   },
 });
